feda1e696f6418478f8c3529459876f76377af82
João Vitor Almeida de Oliveira <joao.almeida200@hotmail.com>
2023-03-04 13:53:16 -0300

    Add custom entrypoint to handle node_modules

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const random = require('random-name');
 
 const app = express();
--- db/ddl/people.sql
+++ db/ddl/people.sql
@@ -1,4 +1,6 @@
-CREATE TABLE people(
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL
+CREATE TABLE people
+(
+    id   INT          NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
 );
